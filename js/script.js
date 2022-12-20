82ef41613b247d92ef14bc20a857584b62c5035b
Kamil Adach <adachkamil98@gmail.com>
2022-12-20 16:17:27 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".js-body");
+let optionalBackgroundColor = document.querySelector(".optionalBackgroundColor");
+let themeName = document.querySelector(".js-themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("optionalBackgroundColor");
+
+    themeName.innerText = body.classList.contains("optionalBackgroundColor") ? "jasny" : "ciemny";
+});
