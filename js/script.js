792741fc68cb90eea03dbc6c811bc93dec49ef12
adachkamil98 <adachkamil98@gmail.com>
2023-01-03 00:14:47 +0100

    let is changed to const, one variable deleted

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 console.log("Hello! What's up?");
 
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let optionalBackgroundColor = document.querySelector(".optionalBackgroundColor");
-let themeName = document.querySelector(".js-themeName");
+{
+const button = document.querySelector(".js-button");
+const body = document.querySelector(".js-body");
+const themeName = document.querySelector(".js-themeName");
 
 button.addEventListener("click", () => {
     body.classList.toggle("optionalBackgroundColor");
 
     themeName.innerText = body.classList.contains("optionalBackgroundColor") ? "jasny" : "ciemny";
 });
+}
