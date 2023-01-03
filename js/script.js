91df41810a79be81a4c214b71db3a29cbb6693b9
adachkamil98 <adachkamil98@gmail.com>
2023-01-03 23:57:38 +0100

    code refactored, functions created

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-console.log("Hello! What's up?");
-
 {
-const button = document.querySelector(".js-button");
-const body = document.querySelector(".js-body");
-const themeName = document.querySelector(".js-themeName");
+    const welcome = () => {
+        console.log("Hello! What's up?")
+    }
 
-button.addEventListener("click", () => {
-    body.classList.toggle("optionalBackgroundColor");
+    welcome();
 
-    themeName.innerText = body.classList.contains("optionalBackgroundColor") ? "jasny" : "ciemny";
-});
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const anotherThemeName = document.querySelector(".js-anotherThemeName");
+
+        body.classList.toggle("body--brighter");
+        anotherThemeName.innerText = body.classList.contains("body--brighter") ? "ciemny" : "jasny";
+    }
+
+    const init = () => {
+        const changeBackgroundColorButton = document.querySelector(".js-changeBackgroundColorButton");
+
+        changeBackgroundColorButton.addEventListener("click", toggleBackground);
+    }
+    
+    init();
 }
